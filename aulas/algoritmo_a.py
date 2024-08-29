62c9af354f40ecf303f1f34ac95d00452c4d174d
tiago <tiagopinzgraffunder@gmail.com>
2024-08-29 20:36:15 -0300

    melhorando o código

--- aulas/algoritmo_a.py
+++ aulas/algoritmo_a.py
@@ -6,33 +6,83 @@ def aplica_operacao(estado, op):
     ncolunas = estado["mapa"]["terreno"].shape[1]
     nlinhas = estado["mapa"]["terreno"].shape[0]
     pos = estado["caminho"][-1]
-    passo = (pos[0], pos[1])
-    if op == "N":
-        if pos[0] > 0:
-            if estado["mapa"]["terreno"][pos[0]-1, pos[1]] < 1:
-                passo = (pos[0]-1, pos[1])
-    if op == "S":
-        if pos[0] < nlinhas-1:
-            if estado["mapa"]["terreno"][pos[0]+1, pos[1]] < 1:
-                passo = (pos[0]+1, pos[1])
-    if op == "L":
-        if pos[1] < ncolunas-1:
-            if estado["mapa"]["terreno"][pos[0], pos[1]+1] < 1:
-                passo = (pos[0], pos[1]+1)
-    if op == "O":
-        if pos[1] > 0:
-            if estado["mapa"]["terreno"][pos[0], pos[1]-1] < 1:
-                passo = (pos[0], pos[1]-1)
+    ops = {"N": (-1, 0), "S": (1, 0), "L": (0, 1), "O": (0, -1)}
+    passo = (pos[0]+ops[op][0], pos[1]+ops[op][1])
     novo_caminho = copy.deepcopy(estado["caminho"])+[passo]
     novo_estado = {"mapa": mapa, "caminho": novo_caminho}
     return novo_estado
-# ---------------------------------------------------
 
 
-estado_ini = {"mapa": mapa, "caminho": [(3, 1)]}
+def get_ops_validas(estado):
+    ncolunas = estado["mapa"]["terreno"].shape[1]
+    nlinhas = estado["mapa"]["terreno"].shape[0]
+    pos = estado["caminho"][-1]
+    ops = {"N": (-1, 0), "S": (1, 0), "L": (0, 1), "O": (0, -1)}
+    ops_validas = []
+    for op in ops:
+        nova_pos = (pos[0]+ops[op][0], pos[1]+ops[op][1])
+        if nova_pos[0] >= 0 and nova_pos[0] < nlinhas and nova_pos[1] >= 0 and nova_pos[1] < ncolunas:
+            if estado["mapa"]["terreno"][nova_pos] < 1 and not (nova_pos in estado["caminho"]):
+                ops_validas.append(op)
+    return ops_validas
+
+
+def verifica_solucao(estado):
+    return estado["caminho"][-1] == estado["mapa"]["saida"]
+
+
+def calc_c(estado):
+    return len(estado["caminho"])
+
+
+def calc_h(estado):
+    s = estado["mapa"]["saida"]
+    p = estado["caminho"][-1]
+    if len(get_ops_validas(estado)) == 0:
+        return float('inf')
+    else:
+        return abs(s[0]-p[0])+abs(s[1]-p[1])
+
+
+def busca_a_estrela(estado_ini, max_niveis):
+    quant_estados = 0
+    folhas = []
+    raiz = {"estado": estado_ini, "ops": [], "f": 0}
+    folhas.append(raiz)
+    niveis = 0
+    while (niveis < max_niveis):
+        niveis = niveis+1
+        melhor_folha = folhas[0]
+        # ----escolhe a melhor folha
+        for folha in folhas:
+            if folha["f"] < melhor_folha["f"]:
+                melhor_folha = folha
+        # ---------------------------
+        folhas.remove(melhor_folha)
+        operacoes = get_ops_validas(melhor_folha["estado"])
+        for op in operacoes:
+            estado = aplica_operacao(melhor_folha["estado"], op)
+            estado["mapa"]["terreno"][estado["caminho"][-1]] = 0.4
+            quant_estados += 1
+            nova_folha = {"estado": estado,
+                          "ops": melhor_folha["ops"]+[op], "f": 0}
+            f = calc_c(nova_folha["estado"])+calc_h(nova_folha["estado"])
+            nova_folha["f"] = f
+            folhas.append(nova_folha)
+            if verifica_solucao(nova_folha["estado"]) == True:
+                return nova_folha["estado"], nova_folha["ops"], quant_estados
+    return None
+
+
+for i in range(mapa["terreno"].shape[0]):
+    for j in range(mapa["terreno"].shape[1]):
+        if mapa["terreno"][i, j] < 1:
+            mapa["terreno"][i, j] = 0
+
+estado_ini = {"mapa": mapa, "caminho": [mapa["entrada"]]}
 mostra_mapa(estado_ini["mapa"], estado_ini["caminho"])
-estado = aplica_operacao(estado_ini, "L")
-estado = aplica_operacao(estado, "L")
-estado = aplica_operacao(estado, "N")
-estado = aplica_operacao(estado, "N")
+max_niveis = 100000
+estado, ops, quant_estados = busca_a_estrela(estado_ini, max_niveis)
 mostra_mapa(estado["mapa"], estado["caminho"])
+print("ops:", ops)
+print("quant estados:", quant_estados)
--- aulas/mapa_a_estrela.py
+++ aulas/mapa_a_estrela.py
@@ -11,21 +11,34 @@ def mostra_mapa(mapa, caminho):
         letras[passo] = "X"
     letras[mapa["entrada"]] = 'E'
     letras[mapa["saida"]] = 'S'
-    plt.figure(figsize=(2, 2))
+    plt.figure(figsize=(3, 3))
     sns.heatmap(mapa['terreno'], annot=letras, fmt="", cbar=False, cmap="Blues",
                 linewidths=0.1, linecolor='black', square=True)
     plt.show()
 
 
-tam_x = 4
-tam_y = 4
+tam_x = 15
+tam_y = 15
 terreno = np.zeros((tam_y, tam_x))
-entrada = (3, 1)
-saida = (0, 0)
+entrada = (14, 6)
+saida = (0, 6)
 mapa = {"terreno": terreno, "entrada": entrada, "saida": saida}
 # obstáculos:
-terreno[3, 0] = 1
-terreno[1, 0] = 1
-terreno[1, 1] = 1
-caminho = [(3, 1), (2, 1), (2, 2)]
+terreno[6, 5] = 1
+terreno[7, 5] = 1
+terreno[8, 5] = 1
+terreno[9, 5] = 1
+terreno[10, 5] = 1
+terreno[11, 5] = 1
+terreno[12, 5] = 1
+terreno[13, 5] = 1
+terreno[6, 7] = 1
+terreno[7, 7] = 1
+terreno[8, 7] = 1
+terreno[9, 7] = 1
+terreno[6, 6] = 1
+terreno[9, 4] = 1
+terreno[9, 3] = 1
+terreno[9, 2] = 1
+caminho = [entrada]
 mostra_mapa(mapa, caminho)
